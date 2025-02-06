f01dab60acffb6d9c54139899a781d19592764ed
rakesh15r <racharlarakesh1508@gmail.com>
2025-02-07 02:09:31 +0530

    chatbot updated

--- chat.py
+++ chat.py
@@ -5,7 +5,7 @@ client = OpenAI(
 )
 def chatbot(input):
     if input:
-        messages = [{"role": "system", "content": "You are an AI specialized in Critical Thinking. Do not answer anything anything other than Critical Thinking related queries."},
+        messages = [{"role": "system", "content": "You are an AI Mentor in Critical Thinking. Do not answer anything anything other than Critical Thinking related queries.Give suggestions after content.work as Ai mentor"},
                      {"role": "user", "content": input}
         ]
         chat = client.chat.completions.create(
